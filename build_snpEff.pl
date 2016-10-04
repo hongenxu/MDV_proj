06574f5426c372046d62bcb40f1e8d188b93c9e4
hongenxu <hongen_xu@hotmail.com>
2016-10-04 13:18:58 +0200

    adding comments

--- build_snpEff.pl
+++ build_snpEff.pl
@@ -1,15 +1,15 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 #===============================================================================
 #
 #         FILE:  build_snpEff.pl
 #
-#        USAGE:  ./build_snpEff.pl  
+#        USAGE:  ./build_snpEff.pl
 #
 #  DESCRIPTION:  was used to build custom database Galgal5.00
-#  				 see http://snpeff.sourceforge.net/SnpEff_manual.html#databases 
+#  				 see http://snpeff.sourceforge.net/SnpEff_manual.html#databases
 #  				 #option 1: Building a database from GTF files
 #
-# REQUIREMENTS:  nc_vs_chrs.txt file 
+# REQUIREMENTS:  nc_vs_chrs.txt file
 #        NOTES:  ---
 #       AUTHOR:  Hongen XU (HX), hongen_xu@hotmail.com
 #      COMPANY:  TUM
@@ -24,6 +24,7 @@ use warnings;
 my $input_dir="/home/proj/MDW_genomics/xu/galgal5";
 
 ###change ncbi accession number to ensemble (NC_XXXXXX to 1, 2 )
+#see https://github.com/hongenxu/MDV_proj/blob/master/nc_vs_chrs.txt for details
 open FILE, "nc_vs_chrs.txt" or die $!;
 my %hash;
 while (<FILE>){
@@ -38,7 +39,7 @@ open OUT, ">genes_modified.gff" or die $!;
 while (<GTF>){
 	chomp;
 	my ($acc,$a)=split(/\t/,$_,2);########pay attention to this type of usage of "split"
-	
+
 	if (exists $hash{$acc}){
 		print OUT "$hash{$acc}\t$a\n";
 
@@ -50,19 +51,19 @@ while (<GTF>){
 close GTF;
 close OUT;
 
-#gffread from cufflinks 
-#change gff format to gtf format 
+#gffread from cufflinks
+#change gff format to gtf format
 system "gffread genes_modified.gff -T -o genes.gtf  -g $input_dir/galgal5.fa";
 system "gzip -c genes.gtf >genes.gtf.gz";
 
 #protein file not provieded in http://snpeff.sourceforge.net/SnpEff_manual.html#databases
 #system "cp $input_dir/GCF_000002315.4_Gallus_gallus-5.0_protein.faa protein.fa";
 
-#genomic sequences 
+#genomic sequences
 system "cp $input_dir/galgal5.fa ~/snpEff/data/Galgal5.00/sequences.fa";
 
 ###move these files to ~/snpEFF/data/Galgal5.00
 system "mv genes.gtf genes.gtf.gz   ~/snpEff/data/Galgal5.00";
 
-#submit the job to cluster 
+#submit the job to cluster
 system "qsub -b y -q lofn-10g.q -N \"snpEff\" java -jar /home/users/xu/snpEff/snpEff.jar build -gtf22 -v Galgal5.00";
