1d6e56420ee2978cd1d3cfc7418236baacdd991a
hongenxu <hongen_xu@hotmail.com>
2017-05-11 10:37:41 +0200

    documentation

--- somatic_scna/get_mappability_of_galgal5.sh
+++ somatic_scna/get_mappability_of_galgal5.sh
@@ -1,12 +1,13 @@
+#create mappability file required for running Control-FREEC
+# the input for this script is galgal5.fa
+#gem library was downloaded from https://sourceforge.net/projects/gemlibrary/ (version 2013-04-06)
 
-wd="/home/proj/MDW_genomics/xu/scna/mappability/"
+#to create a GEM index out of a FASTA file 
+gem-indexer -i galgal5.fa -o galgal5 -T 4
 
-cd ${wd}
-
-gem-indexer -i ../../galgal5/galgal5.fa -o galgal5 -T 4
-
+#to compute the mappability of a reference 
 gem-mappability -I ./galgal5.gem -l 120 -o galgal5.120mer -T 16
 
-gem-2-wig -I galgal5.gem -i galgal5.120mer.mappability  -o mappability 
-
+#change chromsome names, see comments in ./rename_mappability.pl
+perl rename_mappability.pl
 
