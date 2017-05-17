973be0f6c00aff8e7d61c384130f213d8125b59c
hongenxu <hongen_xu@hotmail.com>
2017-05-17 11:22:44 +0200

    update

--- somatic_snv_indel/run_vardict.pl
+++ somatic_snv_indel/run_vardict.pl
@@ -8,8 +8,6 @@ use warnings;
 #https://github.com/AstraZeneca-NGS/VarDict/issues/2
 #here explains why vardict needs input bed files and bed regions
 #recommended to have 150 bp overlap for WGS data to call indels
-#bed file can be found in /home/users/xu/VarDict-1.4.4/bin
-#totally 31 bed files
 
 ###configuration
 my $vardict="/home/users/xu/VarDict-1.4.4/bin";
@@ -17,6 +15,7 @@ my $tumor_dir="/home/proj/MDW_genomics/xu/final_bam/";
 my $normal_dir="/home/proj/MDW_genomics/xu/final_bam/";
 my $output_dir="/scratch/xu/MDV_project/vardict_results";
 my $genome="/home/proj/MDW_genomics/xu/galgal5/galgal5.fa";
+my $bed="/home/users/xu/VarDict-1.4.4/bin/5k_150bpOL_seg.bed";
 
 ##sample identifiers
 #the first tumor match the first normal...
@@ -32,14 +31,14 @@ foreach my $num (0..25){
 
     $tumors[$num]=~/.*(S\d+)/;
     my $sample=$1;
-    foreach my $item (1..31){
-        #see vardict website for explanations for each parameter
-        my $cmd1="$vardict/VarDict -G $genome -b \"$tumor_bam\|$normal_bam\" -th 4 -F 0x500 -z -C -c 1 -S 2 -E 3 -g 4 $vardict/$item.bed  > $output_dir/$sample.vardict.$item";
-        #`qsub -b y -q all.q -l core=4 -N "$sample.$item" "$cmd1"`;
-        my $cmd2="cat $output_dir/$sample.vardict.$item |Rscript $vardict/testsomatic.R |perl $vardict/var2vcf_paired.pl -f 0.01 >$output_dir/$sample.vcf.$item";
-        #`qsub -b y -q all.q -l core=1 -N "$sample.$item" "$cmd2"`;
-        print "$cmd1\n$cmd2\n";
-        }
+    #see vardict website for explanations for each parameter
+    my $cmd1="$vardict/VarDict -G $genome -b \"$tumor_bam\|$normal_bam\" -th 4 -F 0x500 -z -C -c 1 -S 2 -E 3 -g 4 $bed  > $output_dir/$sample.vardict";
+    my $cmd2="cat $output_dir/$sample.vardict |Rscript $vardict/testsomatic.R |perl $vardict/var2vcf_paired.pl -f 0.01 >$output_dir/$sample.vcf";
+    system "cp ~/template.sh $sample.vardict.job";
+    open OUT, ">>$sample.vardict.job" or die $!;
+    print OUT "$cmd1\n$cmd2\n";
+    close OUT;
+    #`qsub -b y -q all.q -l core=4 -N "vardict$sample" "sh $sample.vardict.job"`;
 }
 
 
