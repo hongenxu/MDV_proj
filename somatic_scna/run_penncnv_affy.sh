fab3fba82c3163e515d661c674945a7a59879635
hongenxu <hongen_xu@hotmail.com>
2017-06-26 16:07:27 +0200

    update

--- somatic_scna/run_penncnv_affy.sh
+++ somatic_scna/run_penncnv_affy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+
 #follow guidence at http://penncnv.openbioinformatics.org/en/latest/user-guide/affy/
 #see recommendations for Axiom arrays in the guidence
 #please download penncnv-affy and uncompile in the working directory
@@ -8,26 +14,26 @@
 #####################you many need to change#####################
 
 #working directory
-wd="/home/proj/MDW_genomics/xu/microarray/"
+wd="/home/proj/MDW_genomics/xu/microarray"
 cd ${wd}
 
 #directory for axiom; it includes files specific for this chip design
 axiom_dir="/home/proj/MDW_genomics/xu/microarray/Axiom_67MDSNPs.r1"
 
 #binary file folder for Affymetrix Power Tools
-apt="~/apt_1.19.0/bin/";
+apt="/home/users/xu/apt_1.19.0/bin";
 
 #binary file folder for penncnv-affy
-affy="/home/proj/MDW_genomics/xu/microarray/gw6/bin/"
+affy="/home/proj/MDW_genomics/xu/microarray/gw6/bin"
 
 #binary file folder for PennCNV
-penncnv="/home/users/xu/PennCNV/"
+penncnv="/home/users/xu/PennCNV"
 
 ####################change end######################3
 
 #affymetrxi power tools
 ${apt}/apt-probeset-genotype \
-    --analysis-files-path ${wd} \
+    --analysis-files-path ${axiom_dir} \
     --xml-file ${axiom_dir}/Axiom_67MDSNPs_96orMore_Step1.r1.apt-probeset-genotype.AxiomGT1.xml \
     --summaries \
     --out-dir  ${wd}/output \
@@ -37,7 +43,7 @@ ${apt}/apt-probeset-genotype \
 
 
 # preparing loc file used in the next step
-grep -v "#" ${axiom_dir}/Axiom_67MDSNPs.r1/Axiom_67MDSNPs_Annotation.csv.r1.txt |cut -d"," -f1,4,5 >locfile.txt
+grep -v "#" ${axiom_dir}/Axiom_67MDSNPs_Annotation.csv.r1.txt |cut -d"," -f1,4,5 >locfile.txt
 sed -i 's/"//g' locfile.txt
 sed -i 's/,/\t/g' locfile.txt
 
@@ -56,7 +62,7 @@ perl ${affy}/normalize_affy_geno_cluster.pl ./output/batch1.genocluster \
 cd ./output/
 
 #penncnv
-
+rm MDV.*
 perl ${penncnv}/kcolumn.pl batch1.lrr_baf.txt split 2 -tab -head 3 -name -out MDV --beforestring .CEL
 
 
