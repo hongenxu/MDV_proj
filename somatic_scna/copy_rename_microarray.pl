a306081cd3cb45863a5b027d92c96d2106f7af53
hongenxu <hongen_xu@hotmail.com>
2017-06-26 15:14:16 +0200

    update

--- somatic_scna/copy_rename_microarray.pl
+++ somatic_scna/copy_rename_microarray.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
-#copy CEL files from Alec's folder
+#copy CEL files from Alec's folder to a subfoder /home/proj/MDW_genomics/xu/microarray/CEL
 #rename CEL files to sample identifier
 #generate sexfile.txt required by penncnv-affy
+#microarray.csv file was from Alec, the original file is in excel, and I changed it to csv file
 
 
 use strict;
 use warnings;
+
+chdir "/home/proj/MDW_genomics/xu/microarray/";
 
 open FILE,"microarray.csv" or die $!;
 `rm ./CEL/*.CEL`;
@@ -26,9 +29,9 @@ while (<FILE>){
         $oldname=join("",$a,$b,"_WP-Cheng_101-child-3_P004_",$a,$b);
     }
     print "$oldname\n";
-    my $file=join("","/home/proj/MDW_genomics/affy_microarrays/data/DNALM_CHK_P004/",$oldname,".CEL");
+    my $oldfile=join("","/home/proj/MDW_genomics/affy_microarrays/data/DNALM_CHK_P004/",$oldname,".CEL");
     my $newfile="";
-    if (-e $file){
+    if (-e $oldfile){
         if ($id=~/^017/ or $id=~/^NoT/){
             $newfile="./CEL/$id.CEL";
         }
@@ -37,9 +40,9 @@ while (<FILE>){
         }
     }
     else {
-        print "$file not exists!\n";
+        print "$oldfile not exists!\n";
     }
-    `cp $file $newfile`;
+    `cp $oldfile $newfile`;
     $sex=~s/Female/female/g;
     $sex=~s/Male/male/g;
     $newfile=~s/.\/CEL\///g;
