f61ecbe27c303282357b0902cd878bde1f693685
hongenxu <hongen_xu@hotmail.com>
2017-06-26 11:29:47 +0200

    update

--- somatic_scna/get_mappability_of_galgal5.sh
+++ somatic_scna/get_mappability_of_galgal5.sh
@@ -2,12 +2,17 @@
 # the input for this script is galgal5.fa
 #gem library was downloaded from https://sourceforge.net/projects/gemlibrary/ (version 2013-04-06)
 
+wd="/home/proj/MDW_genomics/xu/scna/mappability/"
+
+cd ${wd}
+
 #to create a GEM index out of a FASTA file 
-gem-indexer -i galgal5.fa -o galgal5 -T 4
+gem-indexer -i /home/proj/MDW_genomics/xu/galgal5/galgal5.fa -o galgal5 -T 4
 
 #to compute the mappability of a reference 
-gem-mappability -I ./galgal5.gem -l 120 -o galgal5.120mer -T 16
+gem-mappability -I ./galgal5.gem -l 120 -o galgal5.120mer -T 4
 
 #change chromsome names, see comments in ./rename_mappability.pl
 perl rename_mappability.pl
+
 
