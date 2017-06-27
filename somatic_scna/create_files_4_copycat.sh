3107ea0f7e7a0cf8747a83b1251469e8f25477d3
hongenxu <hongen_xu@hotmail.com>
2017-06-27 10:43:56 +0200

    first

---
+++ somatic_scna/create_files_4_copycat.sh
@@ -0,0 +1,24 @@
+
+################################create gaps.bed file 
+
+#require bedtools installed
+#ucsc_gaps.bed was downloaded from UCSC table browser, galgal5
+
+grep -v "#" ucsc_gaps.bed |cut -f2-4,8 >tmp
+sed -i '/_/d' tmp
+sed -i 's/chr//g' tmp
+bedtools sort -i tmp >gaps.bed
+rm tmp 
+
+#############################create entrypoints.female and entrypoints.male file
+#create manually
+#the first column is chromosome;
+#the second one is length of this chromosome
+#the third one is the number of this chromosme in female or male
+
+
+
+
+##################create gc and mappability files
+#see /home/proj/MDW_genomics/xu/scna/copycat_anno_galgal5/createCustomAnnotations.v1/ for details
+
