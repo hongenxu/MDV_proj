faa2f6dd68ec1c4dd71832bf5f4a6cb8bffd1c8e
hongenxu <hongen_xu@hotmail.com>
2017-07-07 16:20:37 +0200

    update

--- somatic_scna/create_files_for_FREEC.sh
+++ somatic_scna/create_files_for_FREEC.sh
@@ -1,21 +1,29 @@
 #create  files needed for running control-FREEC
 
 #running this script need a reference genome galgal5.fa,
-#a SNP file Gallus_gallus.vcf
+#and a SNP file Gallus_gallus.vcf
+#Gallus_gallus.vcf wad downloaded from http://ftp.ensembl.org/pub/release-86/variation/vcf/gallus_gallus/Gallus_gallus.vcf.gz
+# require ucsc faSplit installed
 
+
+ref="/home/proj/MDW_genomics/xu/galgal5/galgal5.fa";
+snp="/home/proj/MDW_genomics/xu/Gallus_gallus.vcf"
+faSplit="/home/users/xu/bin/faSplit"
 
 ####create a directory of chromosomes for "chrFiles" parameter in "config_WGS.txt"
 #input file for this part galgal5.fa
 
 
 #working directory
-cd "/home/proj/MDW_genomics/xu/scna/freec"
+wd="/home/proj/MDW_genomics/xu/scna/freec"
+cd ${wd}
+
 
 [ -d ./chromosomes ]  || mkdir ./chromosomes/ #create a directory
 
 
 #faSplit was downloaded from UCSC kentutils
-./faSplit byname /home/proj/MDW_genomics/xu/galgal5/galgal5.fa ./chromosomes/
+${faSplit} byname ${ref} ./chromosomes/
 
 #do not consider contigs not placed on chromsomes
 rm ./chromosomes/NT*
@@ -24,7 +32,7 @@ for i in {1..28} 30 31 32 33 W Z LGE64 MT
 
 do
 
-    mv ./chromsomes/${i}.fa ./chromosomes/chr${i}.fa  #rename file name
+    mv ./chromosomes/${i}.fa ./chromosomes/chr${i}.fa  #rename file name
     sed -i 's/>/>chr/g' ./chromosomes/chr${i}.fa      #rename sequence name inside file
 
 done
@@ -40,12 +48,10 @@ sed -i 's/chrW/chrY/g' ./chromosomes/chrY.fa  #rename sequence name inside file
 ###create a file for "SNPfile" parameter in "config_WGS.txt"
 
 
-#Gallus_gallus.vcf wad downloaded from http://ftp.ensembl.org/pub/release-86/variation/vcf/gallus_gallus/Gallus_gallus.vcf.gz
-
 #header line 
-grep "#" Gallus_gallus.vcf >snp.vcf
+grep "#" ${snp} >snp.vcf
 #only consier SNV postions
-grep -v "#" Gallus_gallus.vcf |grep "TSA=SNV" |awk '{print "chr"$0}'  >>snp.vcf
+grep -v "#" ${snp} |grep "TSA=SNV" |awk '{print "chr"$0}'  >>snp.vcf
 
 sed -i 's/chrW/chrY/g' snp.vcf
 sed -i 's/chrZ/chrX/g' snp.vcf
@@ -63,18 +69,16 @@ gzip snp.vcf  #compress
 
 
 
-
-
 ###create a file used in "samtools mpileup", see 'run_control-freec.pl'
 
-grep -v "#" Gallus_gallus.vcf |grep "TSA=SNV" |grep -v "," |awk '{print $1"\t"$2-1"\t"$2}' >bedfile4Control-freec.bed
+grep -v "#" ${ref} |grep "TSA=SNV" |grep -v "," |awk '{print "chr"$1"\t"$2-1"\t"$2}' >bedfile4Control-freec.bed
 
 
 
 ############create a chromosome length file for "chrLenFile" paramter in "config_WGS.txt"
 
-samtools faidx /home/proj/MDW_genomics/xu/galgal5/galgal5.fa
-cp galgal5.fa.fai chr.tmp
+samtools faidx ${ref}
+cp ${ref}.fai chr.tmp
 sed -i '/NT/d' chr.tmp  #delete contigs not placed on chromosomes
 awk 'BEGIN{i=0} i+=1 {print i"\t""chr"$1"\t"$2}' chr.tmp >chr.len
 
